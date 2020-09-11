6ac0aaa7c0b1f4f70fcdd7323b343ff2b27e0fce
ayushya <ayushyamsr45@gmail.com>
2020-09-11 09:51:35 -0700

    cors changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const PORT=process.env.PORT || 3000;
 const path=require('path');
+cosnt cors=require('cors');
 const connectDB=require('./config/db.js');
 const files=require('./routes/files.js');
 const show=require('./routes/show.js');
@@ -13,6 +14,13 @@ connectDB();
 //express static
 app.use(express.static('public'));
 app.use(express.json());
+
+//Cors
+const corsOption={
+//  origin:['http://localhost:3000','http://localhost:4000','http://localhost:5000']
+  origin:process.env.ALLOWED_CLIENTS.split(',');
+}
+app.use(cors(corsOption));
 
 //template engine
 app.set('views',path.join(__dirname,'/views'));
